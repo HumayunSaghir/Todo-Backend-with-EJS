daece71315c38d25477de1c58d3116becb52c5a6
HumayunSaghir <hr3296726@gmail.com>
2026-02-07 19:40:03 +0500

    removed

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-const {handleShowHomepage, handleShowProfile} = require('../controllers/home')
-
-const router = Router()
-
-router.get('/home', handleShowHomepage)
-router.get('/profile', handleShowProfile)
-
-module.exports = router
